77aaaa778262baf4e0034c5033b61a832f0a7f39
Victor <v.huang435@gmail.com>
2021-01-16 12:43:34 +1100

    added usdc payments

---
+++ contracts/USDCMock.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.7.5;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract USDCMock is ERC20 {
+    constructor() public ERC20("USDC", "US Dollar Coin") {}
+
+    function faucet(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
